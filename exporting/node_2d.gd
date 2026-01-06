88cc246979829c0ab1ea3d285e525793c64949a4
cmj0415 <cmj51406002@gmail.com>
2026-01-06 15:11:05 +0800

    only use export

---
+++ exporting/node_2d.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+@export var height := 10.0
+@export var weight := 50.0
+@export var hp := 100
+@export var atk := 5
+@export var def := 4
+@export var walk_speed := 10.0
+@export var sprint_speed := 15.0
+@export var jmp_height := 2.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
