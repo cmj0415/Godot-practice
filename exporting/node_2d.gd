45dbae9e7d990004c6edd7a0cac8849aa442009a
cmj0415 <cmj51406002@gmail.com>
2026-01-06 15:22:13 +0800

    groups and categories

--- exporting/node_2d.gd
+++ exporting/node_2d.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
 
+@export_category("Body")
 @export var height := 10.0
 @export var weight := 50.0
+
+@export_category("Combat")
+@export_group("Health")
 @export var hp := 100
-@export var atk := 5
-@export var def := 4
+
+@export_group("Attack")
+@export_range(5, 10) var atk := 5
+
+@export_category("Movement")
+@export_group("Horizontal")
 @export var walk_speed := 10.0
 @export var sprint_speed := 15.0
+
+@export_group("Vertical")
 @export var jmp_height := 2.0
 
 # Called when the node enters the scene tree for the first time.
